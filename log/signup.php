aca6f6083d486728770b8b54fd6bbea15d40d9ff
Bhathika nilesh <bhathikanileshkavindya@gmail.com>
2022-12-30 13:45:34 +0530

    30/12----GUI all update

---
+++ log/signup.php
@@ -0,0 +1,74 @@
+
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>SpaceCrew Logging</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="css/style.css">
+
+	</head>
+	<body class="img js-fullheight" style="background-image: url(images/bg2.png);">
+
+
+
+
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 col-lg-4">
+					<div class="login-wrap p-0">
+		      	<h3 class="mb-4 text-center">Don't Have an account?</h3>
+		      	<form action="signup.php" class="signin-form" method="POST">
+		      		<div class="form-group">
+		      			<input id="username" type="username" class="form-control" placeholder="Username" required>
+		      		</div>
+                      <div class="form-group">
+                        <input id="email" type="email" class="form-control" placeholder="Email" required>
+                    </div>
+	            <div class="form-group">
+	              <input id="password" type="password" class="form-control" placeholder="Password" required>
+	              <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+	            </div>
+
+				<div class="form-group">
+					<input id="cpassword" type="cpassword" class="form-control" placeholder="Comfirm Password" required>
+					<span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+				  </div>
+	            <div class="form-group">
+	            	<button type="submit" class="form-control btn btn-primary submit px-3">Sign Up</button>
+	            </div>
+	          </form>
+
+			  <p class="w-100 text-center"> Or Sign In With</p>
+	          <div class="social d-flex text-center">
+	          	<a href="signin.php" class="px-2 py-2 ml-md-1 rounded"><span class="ion-logo-twitter mr-2"></span> Sign In</a>
+	          </div>
+			  <br>
+
+				<div class="social d-flex text-center">
+	          	<a href="../index.html" class="px-2 py-2 ml-md-1 rounded"><span class="ion-logo-twitter mr-2"></span> Go To Home Page</a>
+	          </div>
+
+		      </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="js/jquery.min.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
+
+	</body>
+</html>
+
